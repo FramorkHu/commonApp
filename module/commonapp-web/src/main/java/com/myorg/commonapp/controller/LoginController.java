098e6f74bd7313fdd236765150bd106211f4a338
huyan <huyan@cd.tuan800.com>
2015-06-16 18:32:40 +0800

    spring 基本框架搭建

--- module/commonapp-web/src/main/java/com/myorg/commonapp/controller/LoginController.java
+++ module/commonapp-web/src/main/java/com/myorg/commonapp/controller/LoginController.java
@@ -1,20 +1,22 @@
 package com.myorg.commonapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by huyan on 2015/6/9.
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/etds")
 public class LoginController {
 
     private static final String PREFIX = "/";
 
     @RequestMapping("/")
-    public String login(){
+    public String login(Model model){
 
+        model.addAttribute("aa","aa");
         return PREFIX + "login";
     }
 }
