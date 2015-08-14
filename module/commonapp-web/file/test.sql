2c1dc0505f07309ce5036569265da7dcc6f35fb8
huyan <huyan@cd.tuan800.com>
2015-08-14 09:58:32 +0800

    SQL up

--- module/commonapp-web/file/test.sql
+++ module/commonapp-web/file/test.sql
@@ -1,41 +1,52 @@
-/*
-SQLyog Ultimate v11.24 (64 bit)
-MySQL - 5.5.20 : Database - media
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.24
 
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`media` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `test`;
-
-/*Table structure for table `role_resource` */
+--
+-- Table structure for table `role_resource`
+--
 
 DROP TABLE IF EXISTS `role_resource`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role_resource` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `role_id` int(11) DEFAULT NULL,
   `resource_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `role_resource` */
+--
+-- Dumping data for table `role_resource`
+--
 
-insert  into `role_resource`(`id`,`role_id`,`resource_id`) values (1,1,1),(2,1,2),(3,1,3),(4,2,1),(5,2,2),(6,2,3);
+LOCK TABLES `role_resource` WRITE;
+/*!40000 ALTER TABLE `role_resource` DISABLE KEYS */;
+INSERT INTO `role_resource` VALUES (1,1,1),(2,1,2),(3,1,3),(4,2,1),(5,2,2),(6,2,3);
+/*!40000 ALTER TABLE `role_resource` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `sys_resource` */
+--
+-- Table structure for table `sys_resource`
+--
 
 DROP TABLE IF EXISTS `sys_resource`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_resource` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `parent_id` int(128) DEFAULT NULL COMMENT '父id',
@@ -46,16 +57,26 @@ CREATE TABLE `sys_resource` (
   `resource_desc` varchar(128) DEFAULT NULL COMMENT '资源描述',
   `enable` int(11) DEFAULT NULL COMMENT '0:禁用；1:启用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `sys_resource` */
+--
+-- Dumping data for table `sys_resource`
+--
 
-insert  into `sys_resource`(`id`,`parent_id`,`resource_name`,`resource_permission`,`resource_url`,`priority`,`resource_desc`,`enable`) values (1,NULL,'index',NULL,'/admin/login',1,'添加用户',1),(2,NULL,NULL,NULL,'/admin/user',2,'添加用户',1);
+LOCK TABLES `sys_resource` WRITE;
+/*!40000 ALTER TABLE `sys_resource` DISABLE KEYS */;
+INSERT INTO `sys_resource` VALUES (1,2,'添加用户1','admin:user:addUser','/admin/user/addUser',111,'添加用户',1),(2,3,'用户管理','admin:user:viewUsers','/admin/user/viewUsers',11,'查看用户',1),(3,0,'权限管理','admin:permission','权限管理',1,'权限管理',1);
+/*!40000 ALTER TABLE `sys_resource` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `sys_role` */
+--
+-- Table structure for table `sys_role`
+--
 
 DROP TABLE IF EXISTS `sys_role`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_role` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `role_name` varchar(128) DEFAULT NULL COMMENT '角色名称',
@@ -64,15 +85,25 @@ CREATE TABLE `sys_role` (
   `is_system` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `sys_role` */
+--
+-- Dumping data for table `sys_role`
+--
 
-insert  into `sys_role`(`id`,`role_name`,`role_desc`,`enable`,`is_system`) values (1,'管理员','管理员',1,1),(2,'普通用户','普通用户',1,0);
+LOCK TABLES `sys_role` WRITE;
+/*!40000 ALTER TABLE `sys_role` DISABLE KEYS */;
+INSERT INTO `sys_role` VALUES (1,'管理员','管理员',1,1),(2,'普通用户','普通用户',1,0);
+/*!40000 ALTER TABLE `sys_role` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `user_info` */
+--
+-- Table structure for table `user_info`
+--
 
 DROP TABLE IF EXISTS `user_info`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_info` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(128) DEFAULT NULL,
@@ -80,28 +111,51 @@ CREATE TABLE `user_info` (
   `enabled` int(10) DEFAULT NULL COMMENT '0：禁用，1：启用',
   `is_super_admin` int(11) DEFAULT NULL COMMENT '是否超级用户',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `user_info` */
+--
+-- Dumping data for table `user_info`
+--
 
-insert  into `user_info`(`id`,`user_name`,`password`,`enabled`,`is_super_admin`) values (1,'admin','e6e061838856bf47e1de730719fb2609',1,1),(2,'users','e6e061838856bf47e1de730719fb2609',1,0);
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (1,'admin','e6e061838856bf47e1de730719fb2609',1,1),(2,'users','e6e061838856bf47e1de730719fb2609',1,0),(7,'test','test',NULL,NULL),(9,'test','test',NULL,NULL);
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `user_role` */
+--
+-- Table structure for table `user_role`
+--
 
 DROP TABLE IF EXISTS `user_role`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_role` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `user_role` */
+--
+-- Dumping data for table `user_role`
+--
 
-insert  into `user_role`(`id`,`user_id`,`role_id`) values (1,1,1),(2,2,2);
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,1,1),(2,2,2);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-08-14  9:57:00
